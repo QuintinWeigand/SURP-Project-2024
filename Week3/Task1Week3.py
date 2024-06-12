794d8127cbba2c15ed6e09c3eae86a0d88c8dc3c
Quinn-178 <144628882+Quinn-178@users.noreply.github.com>
2024-06-12 15:23:27 -0400

    updated Task1Week3.py

--- Week3/Task1Week3.py
+++ Week3/Task1Week3.py
@@ -6,23 +6,12 @@ Radius = 1.0e10 #in cm
 global speed_of_light
 speed_of_light = 3.0e10 #in cm/s
 
-def function_1(z_dummy, B_14, P_10, ejecta_mass, ejecta_velocity):
+def function_1(z_dummy, B_14, P_10, ejecta_mass, ejecta_velocity, t_p, t_d, y):
     
     #Note EVERYTHING at this point should be CONVERTED to CGS
-    
-    #Getting all the necessary variables for function_1
-
-    t_p = 1.3 * B_14**-2 * P_10**2 #this is in years
-    t_p = t_p * 31536000 #converting years to seconds
-    
-    t_d = np.sqrt(( 2 *.33 * ejecta_mass) / (13.8 * speed_of_light * ejecta_velocity) )
-
-    y = t_d / t_p
 
     #Math for function_1
-    
-    #print("z_dummy is:", z_dummy)
-    #print("part1 is:", part1)
+
     part1 = np.exp(z_dummy**2 + ((Radius * z_dummy) / (ejecta_velocity * t_d)))
 
     part2 = ( ((Radius) / (ejecta_velocity * t_d)) + z_dummy )
@@ -50,13 +39,9 @@ def simpson(ts_ex1, vs_ex1):
     #Returns the final area (the integral) to the calling environment
     return area_Simp
 
-def return_magnetar_luminosity(t, B_14, P_10, ejecta_mass, ejecta_velocity):
+def return_magnetar_luminosity(t, B_14, P_10, ejecta_mass, ejecta_velocity, t_p, t_d, y, E_p):
     #Converts days to seconds
     t = t * 86400
-
-    #Calcualting t_d
-    t_d = np.sqrt(( 2 *.33 * ejecta_mass) / (13.8 * speed_of_light * ejecta_velocity) )
-    #print("t_d is:", t_d)
     
     #Calculating x for the integral upper bound
     x = t / t_d
@@ -65,23 +50,17 @@ def return_magnetar_luminosity(t, B_14, P_10, ejecta_mass, ejecta_velocity):
     x_prime = np.linspace(0, x, 1000)
 
     #Generates a list of data for function_1 to then be sent to simpson
-    list1 = np.array([function_1(z_dummy, B_14, P_10, ejecta_mass, ejecta_velocity) for z_dummy in x_prime])
+    list1 = np.array([function_1(z_dummy, B_14, P_10, ejecta_mass, ejecta_velocity, t_p, t_d, y) for z_dummy in x_prime])
 
     #Generates the integral from a list of data points
     integral_1 = simpson(x_prime, list1)
 
-    #Calculating important variables for the rest of the function
-    t_p = 1.3 * B_14**-2 * P_10**2 #this is in years
-    t_p = t_p * 31536000 #converting years to seconds
-    #print("t_p is:", t_p)
-
-    E_p = 2e50 / ( P_10**2 )
-    #print("E_p is:", E_p)
-
+    #Calculating the individual parts for the magnetar luminosity
     part1 = ((2 * E_p) / t_p) * np.exp( -( (t**2 / t_d**2) + ( (Radius * t) / (ejecta_velocity * t_d**2) ) ) )
 
     part2 = integral_1
 
+    #Finally Calcualting the final magnetar luminosity
     luminosity = part1 * part2
 
     return luminosity
@@ -98,9 +77,21 @@ ejecta_velocity = ejecta_velocity * 100000
 ejecta_mass = float(input("Please enter an ejecta mass (in solar mass): "))
 ejecta_mass = ejecta_mass * 1.989e33
 
-x_list = np.linspace(1,200, 398)
-y_list = np.array([return_magnetar_luminosity(t, B_14, P_10, ejecta_mass, ejecta_velocity) for t in x_list])
+#Calculating important variables that are used within the functions above
+t_p = 1.3 * B_14**-2 * P_10**2 #this is in years
+t_p = t_p * 31536000 #converting years to seconds
 
+t_d = np.sqrt(( 2 *.33 * ejecta_mass) / (13.8 * speed_of_light * ejecta_velocity) )
+
+y = t_d / t_p
+
+E_p = 2e50 / ( P_10**2 )
+
+#Generating the lists of x points and y points
+x_list = np.linspace(1,200, 398)
+y_list = np.array([return_magnetar_luminosity(t, B_14, P_10, ejecta_mass, ejecta_velocity, t_p, t_d, y, E_p) for t in x_list])
+
+#Printing the y list for comparison (most of the time commented out)
 print(y_list)
 
 #Creates the plot
@@ -111,9 +102,9 @@ ax.plot(x_list, y_list,
         linewidth = 2)
 
 #Sets the title and x & y labels
-ax.set_title('Luminosity ( L(t) ) vs. Time (Days)')
+ax.set_title('Magnetar Luminosity ( L(t) ) vs. Time (Days)')
 ax.set_xlabel('Time (Days)')
-ax.set_ylabel('Luminosity ( L(t) )')
+ax.set_ylabel('Magnetar Luminosity ( L(t) )')
 
 #Displays the plot
-#plt.show()
+plt.show()
