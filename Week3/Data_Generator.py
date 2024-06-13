8ae245c4fd8043012e294c9af29b8dd5636a8ba2
Quinn-178 <144628882+Quinn-178@users.noreply.github.com>
2024-06-13 10:02:42 -0400

    Add files via upload

--- Week3/Data_Generator.py
+++ Week3/Data_Generator.py
@@ -65,112 +65,71 @@ def return_magnetar_luminosity(t, B_14, P_10, ejecta_mass, ejecta_velocity, t_p,
 
     return luminosity
 
-
-#B_14 = float(input("Please enter B (in 1e14G): "))
-
-#P = float(input("Please enter P (in ms): "))
-#P_10 = P / 10
-
-#ejecta_velocity = int(input("Please enter a ejecta velocity (in km/s): "))
-#ejecta_velocity = ejecta_velocity * 100000
-
-#ejecta_mass = float(input("Please enter an ejecta mass (in solar mass): "))
-#ejecta_mass = ejecta_mass * 1.989e33
-
-#Calculating important variables that are used within the functions above
-#t_p = 1.3 * B_14**-2 * P_10**2 #this is in years
-#t_p = t_p * 31536000 #converting years to seconds
-
-#t_d = np.sqrt(( 2 *.33 * ejecta_mass) / (13.8 * speed_of_light * ejecta_velocity) )
-
-#y = t_d / t_p
-
-#E_p = 2e50 / ( P_10**2 )
-
-#Generating the lists of x points and y points
-#x_list = np.linspace(1,200, 398)
-#y_list = np.array([return_magnetar_luminosity(t, B_14, P_10, ejecta_mass, ejecta_velocity, t_p, t_d, y, E_p) for t in x_list])
-
-#Printing the y list for comparison (most of the time commented out)
-#print(y_list)
-
-#Creates the plot
-#fig, ax = plt.subplots()
-#ax.plot(x_list, y_list,
-#        linestyle = '-',
-#        color = 'black',
-#        linewidth = 2)
-
-#Sets the title and x & y labels
-#ax.set_title('Magnetar Luminosity ( L(t) ) vs. Time (Days)')
-#ax.set_xlabel('Time (Days)')
-#ax.set_ylabel('Magnetar Luminosity ( L(t) )')
-
-#Displays the plot
-#plt.show()
-
+#Creating our arrays for data we wish to be entered
 B_14 = np.array([0.1, 0.3, 0.5, 0.7, 0.9, 1.0, 3.0, 5.0, 7.0, 9.0, 10.0, 30.0, 50.0, 70.0, 90.0, 100.0])
 P_10 = np.array([1.0, 3.0, 5.0, 7.0, 9.0, 10.0, 30.0, 50.0, 70.0, 90.0, 100.0])
 ejecta_velocity = np.array([1000.0, 3000.0, 5000.0, 7000.0, 9000.0, 10000.0, 13000.0, 15000.0, 17000.0, 19000.0, 20000.0])
 ejecta_mass = np.array([2.0, 5.0, 10.0])
 
+total_files = len(B_14) * len(P_10) * len(ejecta_velocity) * len(ejecta_mass)
 
-#Converting all the arrays to CGS
+#Converting all the arrays to CGS (to avoid conflict in final iteration)
 for i in range(len(P_10)):
     P_10[i] = P_10[i] / 10
 for j in range(len(ejecta_velocity)):
     ejecta_velocity[j] = ejecta_velocity[j] * 100000
 for k in range(len(ejecta_mass)):
     ejecta_mass[k] = ejecta_mass[k] * 1.989e33
-print(ejecta_mass)
 
+#Creates the x_list (static for entire program)
+x_list = np.linspace(1, 200, 398)
 
-#B_14 = [0.1, 0.3, 0.5, 0.7, 0.9, 1.0, 3.0, 5.0, 7.0, 9.0, 10.0, 30.0, 50.0, 70.0, 90.0, 100.0]
-#P = [1.0, 3.0, 5.0, 7.0, 9.0, 10.0, 30.0, 50.0, 70.0, 90.0, 100.0]
-#ejecta_velocity = [1000.0, 3000.0, 5000.0, 7000.0, 9000.0, 10000.0, 13000.0, 15000.0, 17000.0, 19000.0, 20000.0]
-#ejecta_mass = [2.0, 5.0, 10.0]
-
-
-x_list = np.linspace(1,200, 398)
-
+#Creates and initilized our counter that will be useful for observing during runtime
 counter = 1
 
 for i in range(len(B_14)):
     
     for j in range(len(P_10)):
 
+        #Calculates important variables
         E_p = 2e50 / ( P_10[j]**2 )
         t_p = 1.3 * B_14[i]**-2 * P_10[j]**2 #this is in years
         t_p = t_p * 31536000 #converting years to seconds
         for k in range(len(ejecta_velocity)):
 
             for l in range(len(ejecta_mass)):
-
+                
+                #Creates filename based on entry data
                 filename = "M_LC_" + str("{:.2f}".format(B_14[i])) + "_" + str("{:.1f}".format(P_10[j] * 10)) + "_" + str(ejecta_velocity[k] / 100000) + "_" + str(ejecta_mass[l] / 1.989e33) + ".data"
 
+                #Opens file
                 file = open(filename, "w")
 
-
-                print("Generating file:", counter ,"/", (len(B_14) * len(P_10) * len(ejecta_velocity) * len(ejecta_mass)), " ("+ "{:.2f}".format((counter / (len(B_14) * len(P_10) * len(ejecta_velocity) * len(ejecta_mass))) * 100)+ "%)" )
+                #Outputs to the console file number of total files and percentage
+                print("Generating file:", counter ,"/", total_files, " ("+ "{:.2f}".format((counter / (total_files)) * 100)+ "%)" )
                 
-
+                #Calculates important variables for program
                 t_d = np.sqrt(( 2 *.33 * ejecta_mass[l]) / (13.8 * speed_of_light * ejecta_velocity[k]) )
 
                 y = t_d / t_p
 
+                #Generates the y_list
                 y_list = np.array([return_magnetar_luminosity(t, B_14[i], P_10[j], ejecta_mass[l], ejecta_velocity[k], t_p, t_d, y, E_p) for t in x_list])
 
-
+                #Writes the x_list and the y_list to the file opened
                 for m, (first, second) in enumerate(zip(x_list, y_list)):
                     file.write(str("{:3.1f}".format(first)) + "    " + str("{:.5e}".format(second)) + '\n')
 
+                #Closes the file
                 file.close()
 
+                #Important runtime data to keep track
                 print("Instances used:")
                 print(B_14[i])
                 print(P_10[j] * 10)
                 print(ejecta_velocity[k] / 100000)
                 print(ejecta_mass[l] /  1.989e33)
 
+                #Increments counter
                 counter += 1
             
