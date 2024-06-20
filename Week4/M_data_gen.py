d530d2fafe7f54f85dcb5929190340c92db66050
Quinn-178 <144628882+Quinn-178@users.noreply.github.com>
2024-06-20 12:10:45 -0400

    Add files via upload

---
+++ Week4/M_data_gen.py
@@ -0,0 +1,138 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+#list1 = os.listdir()
+#list1.remove("testing.py") #or the filename of the python code
+
+def read_data_from_file(filename):
+    x_list = []
+    y_list = []
+    
+    #I belive since we are using "as" then we do not need to close the file but just in case 
+    #I will close the file below
+    with open(filename, 'r') as file:
+        for line in file:
+            #Splits the line into two
+            values = line.split()
+            if len(values) == 2:
+                x_list.append(float(values[0]))
+                y_list.append(float(values[1]))
+
+    #Makes sure the file that is being read is closed
+    file.close()
+    
+    return x_list, y_list
+
+def coef_of_variation(standard_deviation, mean):
+    return (standard_deviation / mean)
+
+def skew(y_list, mean, standard_deviation):
+    
+    sum = 0
+
+    for i in range(len(y_list)):
+        sum += ( (y_list[i] - mean)**3 ) 
+
+    skew = sum / ( (len(y_list) - 1) * (standard_deviation**3) )
+    
+    return skew
+
+def kurtosis(y_list, mean):
+    
+    top_sum = 0
+    bot_sum = 0
+    n = len(y_list)
+
+    for i in range(len(y_list)):
+        top_sum += ( (y_list[i] - mean)**4 ) 
+        bot_sum += ( (y_list[i] - mean)**2 )
+    
+    #print("top sum =", top_sum, "\nbot sum =", bot_sum)
+
+    kurtosis = (n * top_sum) / (bot_sum**2)
+    
+    return kurtosis
+
+def MAD(y_list, median):
+    
+    temp_list = []
+    
+    for i in range(len(y_list)):
+        temp_list.append(np.absolute(y_list[i] - median))
+    
+    #print("Printing temp list:\n", temp_list)
+    
+    return np.median(temp_list)
+
+#Temporary will use os.listdict() for handeling the filenames and iterate through
+#filename = "LC_0.8_5_1000.0.data"
+
+
+
+filelist = os.listdir()
+filelist.remove("M_data_gen.py")
+filelist.remove("data_reader.py")
+
+
+file = open("data_sheet.data", "w")
+
+for i in range(len(filelist)):
+
+    B = None
+    P = None
+    V = None
+    M = None
+
+    print("Filename for current instance:", filelist[i])
+    filename_elements = filelist[i].split("_")
+    
+
+    #This is so the testing files I make do not break the program, this can be removed shortly once everything is set
+    if (len(filename_elements) > 2):
+        filename_elements[-1] = filename_elements[-1].removesuffix(".data")
+        
+        
+        #print(filename_elements)
+
+        #Reads the variables for the data used into memory can be used 
+        #which will be used to output to a file
+        B = filename_elements[2]
+        P = filename_elements[3]
+        V = filename_elements[4]
+        M = filename_elements[5]
+
+
+    x_list, y_list = read_data_from_file(filelist[i])
+
+    #print(y_list)
+
+    #defines important non changing varaibles (max, standard deviation, mean, etc.)
+    maximum = np.max(y_list)
+    mean = np.mean(y_list)
+    median = np.median(y_list)
+    standard_deviation = np.std(y_list)
+
+    #print("Maximum =", maximum)
+    #print("Mean =", mean)
+    #print("Median =", median)
+    #print("Standard Deviation =", standard_deviation)
+
+    #print("\nThe five important data points:")
+    #print("The maximum peak =", maximum)
+    #print("Coefficient Of Variation =", coef_of_variation(standard_deviation, mean))
+    #print("Skew =", skew(y_list, mean, standard_deviation))
+    #print("Kurtosis =", kurtosis(y_list, mean))
+    #print("Median Absolute Deviation =", MAD(y_list, median))
+
+    file.write(str(B) + " " + str(P) + " " + str(V) + " " + str(M) + " ")
+    file.write(str(maximum) + " " + str(coef_of_variation(standard_deviation, mean)) + " " + str(skew(y_list, mean, standard_deviation)) + " ")
+    file.write(str(kurtosis(y_list, mean)) + " " + str(MAD(y_list, median)) + "\n")
+
+
+file.close()
+
+#fig, ax = plt.subplots()
+#ax.plot(x_list, y_list, linestyle = '-', color = 'black', linewidth = 2)
+
+#plt.show()
