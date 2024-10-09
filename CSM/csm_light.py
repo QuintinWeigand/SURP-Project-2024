7f708428b81a24cb88e68e2e8c79805177baac50
Quintin Weigand <144628882+QuintinWeigand@users.noreply.github.com>
2024-10-09 15:59:06 -0400

    Add files via upload

--- CSM/csm_light.py
+++ CSM/csm_light.py
@@ -57,21 +57,30 @@ def calcualteESN(v_sn, massOfEjecta):
     ESN = (((3 * (N-3) / (10 * (N-5)))) * v_sn**2 * massOfEjecta)
     return ESN
 
+def calculateVSN(esn, massOfEjecta):
+    vsn = np.sqrt( (10 * (N-5) * esn) / (3 * (N-3) * massOfEjecta) )
+    return vsn
+
 def calculateT_I(VSN, radius_p):
     return (radius_p / VSN)
 
 def calcualteG_N(ESN, massOfEjecta):
     G_N = (1 / (4 * np.pi * (DELTA - N))) * ( ( (2 * (5 - DELTA) * (N-5) * ESN)**((N-3)/2) ) / ((3 - DELTA) * (N-3) * massOfEjecta)**((N-5) / 2))
-    return G_N
+    return abs(G_N)
 
 def calculateT_FS_STAR(q, m_csm_th, g_n):
     funkyPower = ((N-S) / (N-3) * (3-S))
-    t_fs_star = (np.abs( ((3-S) * q**((3-N)/(N-S)) * (A * g_n)**((S-3) / (N-S))) / (4 * np.pi * B_F**(3-S))))**(funkyPower) * m_csm_th**(funkyPower)
+    #print((A * g_n)**((S-3) / (N-S)))
+    #print((3-S) * q**((3-N)/(N-S)), (A * g_n)**((S-3) / (N-S)))
+    #print((np.abs( ((3-S) * q**((3-N)/(N-S)) * (A * g_n)**((S-3) / (N-S))) / (4 * np.pi * B_F**(3-S))))) #NOTE:THIS RESULTS IN NAN CURRENTLY
+    t_fs_star = (np.abs( ( (3-S) * q**((3-N)/(N-S)) * (A * g_n)**((S-3) / (N-S)) ) / (4 * np.pi * B_F**(3-S))))**(funkyPower) * m_csm_th**(funkyPower)
+    #print('t_fs_star', q, m_csm_th, funkyPower, g_n, t_fs_star)
     return t_fs_star
 
 def calculateT_RS_STAR(v_sn, g_n, q, massOfEjecta):
     t_rs_star = ( ( ( (v_sn) / ((B_R) * ((A * g_n) / q))**(1/(N-S)) ) * 
                  (1 - ((3-N) * massOfEjecta) / (4 * np.pi * (v_sn)**(3-N) * g_n)))**(1/(3-N)) )**((N-S) / (S-3))
+    #print('t_rs_star', v_sn, g_n, q, massOfEjecta, t_rs_star)
     return t_rs_star
 
 def stepFunction(x):
@@ -80,7 +89,7 @@ def stepFunction(x):
         returnValue = 1
     return returnValue
 
-def function_1(t_prime, t_0, t_i, g_n, q):
+def function_1(t_prime, t_0, t_i, g_n, q, m_csm_th, v_sn, ejectaMass):
     # t_0 = calculateT_0(m_csm = 0, r_ph = 0)
     # t_i = calculateT_I(VSN=0, radius_p=0) #NOTE: WE CALCULATED THIS OUTSIDE OF SCOPE (THIS IS TEMPORARY)
     # g_n = calcualteG_N(ESN=0, massOfEjecta=0) #NOTE ESN WAS CALCULATED OUT OF SCOPE #NOTE G_N WAS CALCULATED OUT OF SCOPE  (TEMP)
@@ -88,8 +97,8 @@ def function_1(t_prime, t_0, t_i, g_n, q):
 
     instance = (
         ( (2 * np.pi) / (N - S)**3 ) * g_n**( (5-S) / (N-S) ) * q**( (N-5) / (N-S) ) * (N-3)**2 * (N-5) * B_F**(5-S) *
-        A**( (5-S) / (N-S) ) * (t_prime + t_i)**( (2*N + 6 * S - N*S - 15) / (N-S) ) * stepFunction(calculateT_FS_STAR(q=0,m_csm_th=0,g_n=0) - t_prime) + 2 * np.pi * ( (A*g_n) / (q) )**( (5-N) / (N-S) ) *
-        B_R**(5-N) * g_n * ( (3-S) / (N-S) )**3 * (t_prime + t_i)**( (2*N + 6 * S - N*S - 15) / (N-S) ) * stepFunction(calculateT_RS_STAR(v_sn=0,g_n=0,q=0) - t_prime)
+        A**( (5-S) / (N-S) ) * (t_prime + t_i)**( (2*N + 6 * S - N*S - 15) / (N-S) ) * stepFunction(calculateT_FS_STAR(q, m_csm_th, g_n) - t_prime) + 2 * np.pi * ( (A*g_n) / (q) )**( (5-N) / (N-S) ) *
+        B_R**(5-N) * g_n * ( (3-S) / (N-S) )**3 * (t_prime + t_i)**( (2*N + 6 * S - N*S - 15) / (N-S) ) * stepFunction(calculateT_RS_STAR(v_sn, g_n, q, ejectaMass) - t_prime)
     )    
 
 
@@ -118,7 +127,7 @@ def returnCSMLuminosity(t, ejectaMass, v_sn, massNI, massCSM, r_p, csm_radius, e
     x_prime = np.linspace(0, t ,1000)
     #We need a list for each integral (2 total lists)
     #List 1
-    list_1 = np.array([function_1(t_prime, t_0, t_0_prime, g_n, q) for t_prime in x_prime])
+    list_1 = np.array([function_1(t_prime, t_0, t_0_prime, g_n, q, m_csm_th, v_sn, ejectaMass) for t_prime in x_prime])
     list_2 = np.array([function_2(t_prime, ejectaMass, r_ph, massNI, m_csm_th, t_0_prime) for t_prime in x_prime])
 
     integral_1 = simpson(x_prime, list_1)
@@ -132,8 +141,8 @@ def returnCSMLuminosity(t, ejectaMass, v_sn, massNI, massCSM, r_p, csm_radius, e
     return luminosity
 
 
+#x_list = np.linspace(1,200,398)
 x_list = np.linspace(1,200,398)
-
 
 #These are temporary representing the inevitable inputs (I am acting as if I had the inputs right now)
 # ejectaMass = 0
@@ -144,20 +153,20 @@ x_list = np.linspace(1,200,398)
 # radiusCSM = 0
 
 #These need to be converted first
-ejectaMass = [] 
-v_sn = []
-massNI = []
-massCSM = []
-r_p = []
-radiusCSM = []
+ejectaMass = [12] 
+v_sn = [calculateVSN(1e51, (12 * 1.989e33))]
+massNI = [0.08]
+massCSM = [1]
+r_p = [1.0e14]
+radiusCSM = [1.14e15]
 
 # Conversions
 for i in range(len(ejectaMass)):
     ejectaMass[i] = ejectaMass[i] * 1.989e33
-for j in range(len(v_sn)):
-    v_sn[j] = v_sn[j] * 100000
+# for j in range(len(v_sn)):
+#     v_sn[j] = v_sn[j] * 100000
 for k in range(len(massCSM)):
-    massCSM[j] = massCSM[j] * 1.989e33
+    massCSM[k] = massCSM[k] * 1.989e33
 for l in range(len(massNI)):
     massNI[l] = massNI[l] * 1.989e33
 
@@ -180,13 +189,13 @@ for e_mass in range(len(ejectaMass)):
                         #TODO: WE NEED TO PASS THESE TO THE returnCSMLuminosity FUNCTION STILL 
                         q = calcualteQ(massCSM[csm_mass], radiusCSM[csm_radius], r_p[radius_p])
                         r_ph = calculateR_ph(q, radiusCSM[csm_radius])
-                        m_csm_th = calculateM_CSM_TH(q, r_ph, r_p)
+                        m_csm_th = calculateM_CSM_TH(q, r_ph, r_p[radius_p])
                         t_0 = calculateT_0(m_csm_th, r_ph)
                         t_0_prime = (K * (ejectaMass[e_mass] + m_csm_th)) / (BETA * SPEED_OF_LIGHT * r_ph) 
 
 
                         y_list = np.array([returnCSMLuminosity(t, ejectaMass[e_mass], v_sn[vel_sn], massNI[ni_mass], 
-                                                               massCSM[csm_mass], r_p[radius_p], csm_radius[radiusCSM], 
+                                                               massCSM[csm_mass], r_p[radius_p], radiusCSM[csm_radius], 
                                                                esn, g_n, q, r_ph, m_csm_th, t_0, t_0_prime) for t in x_list])
 
 
@@ -197,17 +206,17 @@ for e_mass in range(len(ejectaMass)):
 #NOTE: All I need is to verify what values are being passed in and what 
 #      to copy from the nickel decay model. It seems like a lot but I believe it really isn't
 
-#This is bogus for not bull I'll just have it commented out.
-# fig, ax = plt.subplots()
-# ax.plot(x_list, y_list,
-#         linestyle = '-',
-#         color = 'black',
-#         linewidth = 2)
+# This is bogus for not bull I'll just have it commented out.
+fig, ax = plt.subplots()
+ax.plot(x_list, y_list,
+        linestyle = '-',
+        color = 'black',
+        linewidth = 2)
 
-# ax.set_title('Luminosity ( L(t) ) vs. Time (Days)')
-# ax.set_xlabel('Time (Days)')
-# ax.set_ylabel('Luminosity ( L(t) )')
+ax.set_title('Luminosity ( L(t) ) vs. Time (Days)')
+ax.set_xlabel('Time (Days)')
+ax.set_ylabel('Luminosity ( L(t) )')
 
-# #Displays the plot
-# plt.show()
+#Displays the plot
+plt.show()
 
