6126eb080c64364de12253ab399fb2efeb7efb06
Quintin Weigand <quinnweigand178@gmail.com>
2024-10-16 15:42:48 -0400

    updated .gitignore / Data_Generator.py

--- Week3/Data_Generator.py
+++ Week3/Data_Generator.py
@@ -69,14 +69,14 @@ def return_magnetar_luminosity(t, B_14, P_10, ejecta_mass, ejecta_velocity, t_p,
 
 # TODO: B_14 is going to be replaced with t_p (NOTE Shing Chi gave parameters in days (convert to seconds))
 # TODO: We have to calculate B_14 values based on our t_p
-B_14 = np.array([0.1, 0.3, 0.5, 0.7, 0.9, 1.0, 3.0, 5.0, 7.0, 9.0, 10.0, 30.0, 50.0, 70.0, 90.0, 100.0])
+# B_14 = np.array([0.1, 0.3, 0.5, 0.7, 0.9, 1.0, 3.0, 5.0, 7.0, 9.0, 10.0, 30.0, 50.0, 70.0, 90.0, 100.0])
 
-
+t_p = np.array([5.0, 10.0, 30.0, 50.0, 70.0, 100.0]) # NOTE: This is in days
 P_10 = np.array([1.0, 3.0, 5.0, 7.0, 9.0, 10.0, 30.0, 50.0, 70.0, 90.0, 100.0])
 ejecta_velocity = np.array([1000.0, 3000.0, 5000.0, 7000.0, 9000.0, 10000.0, 13000.0, 15000.0, 17000.0, 19000.0, 20000.0])
 ejecta_mass = np.array([2.0, 5.0, 10.0])
 
-total_files = len(B_14) * len(P_10) * len(ejecta_velocity) * len(ejecta_mass)
+total_files = len(t_p) * len(P_10) * len(ejecta_velocity) * len(ejecta_mass)
 
 #Converting all the arrays to CGS (to avoid conflict in final iteration)
 for i in range(len(P_10)):
@@ -85,30 +85,40 @@ for j in range(len(ejecta_velocity)):
     ejecta_velocity[j] = ejecta_velocity[j] * 100000
 for k in range(len(ejecta_mass)):
     ejecta_mass[k] = ejecta_mass[k] * 1.989e33
+for l in range(len(t_p)):
+    t_p[l] = float(t_p[l]) / 365
 
 #Creates the x_list (static for entire program)
 x_list = np.linspace(1, 200, 398)
 
 #Creates and initilized our counter that will be useful for observing during runtime
+
+directory = r"/home/quinn/Desktop/Shing-Chi/Week3/MagnetarDB/"
+
 counter = 1
 
-for i in range(len(B_14)):
+for i in range(len(t_p)):
     
     for j in range(len(P_10)):
 
+        # calculate B14
+        print("t_p: ", t_p[0])
+        B_14 = np.sqrt( (.3 * P_10[j]) / t_p[i] )
+        t_p_sec = t_p[i] * 3.154e+7
+
         #Calculates important variables
         E_p = 2e50 / ( P_10[j]**2 )
-        t_p = 1.3 * B_14[i]**-2 * P_10[j]**2 #this is in years
-        t_p = t_p * 31536000 #converting years to seconds
         for k in range(len(ejecta_velocity)):
 
             for l in range(len(ejecta_mass)):
                 
                 #Creates filename based on entry data
-                filename = "M_LC_" + str("{:.2f}".format(B_14[i])) + "_" + str("{:.1f}".format(P_10[j] * 10)) + "_" + str(ejecta_velocity[k] / 100000) + "_" + str(ejecta_mass[l] / 1.989e33) + ".data"
+                filename = "M_LC_" + str("{:.2f}".format(t_p[i] * 365)) + "_" + str("{:.1f}".format(P_10[j] * 10)) + "_" + str(ejecta_velocity[k] / 100000) + "_" + str(ejecta_mass[l] / 1.989e33) + ".data"
+
+                finalLocation = directory + filename
 
                 #Opens file
-                file = open(filename, "w")
+                file = open(finalLocation, "w")
 
                 #Outputs to the console file number of total files and percentage
                 print("Generating file:", counter ,"/", total_files, " ("+ "{:.2f}".format((counter / (total_files)) * 100)+ "%)" )
@@ -116,10 +126,10 @@ for i in range(len(B_14)):
                 #Calculates important variables for program
                 t_d = np.sqrt(( 2 *.33 * ejecta_mass[l]) / (13.8 * speed_of_light * ejecta_velocity[k]) )
 
-                y = t_d / t_p
+                y = t_d / t_p_sec
 
                 #Generates the y_list
-                y_list = np.array([return_magnetar_luminosity(t, B_14[i], P_10[j], ejecta_mass[l], ejecta_velocity[k], t_p, t_d, y, E_p) for t in x_list])
+                y_list = np.array([return_magnetar_luminosity(t, B_14, P_10[j], ejecta_mass[l], ejecta_velocity[k], t_p_sec, t_d, y, E_p) for t in x_list])
 
                 #Writes the x_list and the y_list to the file opened
                 for m, (first, second) in enumerate(zip(x_list, y_list)):
@@ -130,7 +140,7 @@ for i in range(len(B_14)):
 
                 #Important runtime data to keep track
                 print("Instances used:")
-                print(B_14[i])
+                print(t_p[i] * 365)
                 print(P_10[j] * 10)
                 print(ejecta_velocity[k] / 100000)
                 print(ejecta_mass[l] /  1.989e33)
